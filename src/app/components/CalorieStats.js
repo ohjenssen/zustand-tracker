2dabed3b9a3162758979adbda8e3e67674fbfaf6
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-04 19:15:50 +0200

    Added dynamic calorie-need

--- src/app/components/CalorieStats.js
+++ src/app/components/CalorieStats.js
@@ -1,28 +1,26 @@
-import { useEffect } from "react";
-import { useUserStore } from "../store/store";
+import { useAuthStore } from "../store/store";
 import Spinner from "./Spinner";
 
 export default function CalorieStats({ eaten }) {
-  const fetchUser = useUserStore((state) => state.fetchUser);
-  useEffect(() => {
-    fetchUser()
-  }, [fetchUser])
-  const dailyCalories = useUserStore((state) => state.dailyCalories)
+    const user = useAuthStore((state) => state.user);
 
-  const progressPercent = Math.min((eaten / dailyCalories) * 100, 100);
+    const proteins_in_calories = user?.gram_protein_need * 4;
+    const fat_in_calories = user?.gram_fat_need * 9;
+    const carbohydrates_in_calories = user?.gram_carbohydrate_need * 4;
 
-  return (
-    <section className="mt-8 mb-8">
-      <h2 className="text-xl font-medium mb-1 text-[#00ffb3]">Eaten</h2>
-      <div className="text-2xl font-bold mb-4 text-white">
-        {eaten} / <span className="text-gray-300 text-lg font-normal">{dailyCalories ? dailyCalories : <Spinner size='sm'/>} kcal</span>
-      </div>
-      <div className="w-full h-3 bg-white/20 rounded-full overflow-hidden">
-        <div 
-          className="h-full bg-yellow-400 transition-all duration-500" 
-          style={{ width: `${progressPercent}%` }}
-        ></div>
-      </div>
-    </section>
-  );
+    const dailyCalories = proteins_in_calories + fat_in_calories + carbohydrates_in_calories;
+
+    const progressPercent = Math.min((eaten / dailyCalories) * 100, 100);
+
+    return (
+        <section className="mb-8">
+            <h2 className="text-xl font-medium mb-1 text-[#00ffb3]">Eaten</h2>
+            <div className="text-2xl font-bold mb-4 text-white">
+                {eaten} / <span className="text-gray-300 text-lg font-normal">{dailyCalories ? dailyCalories : <Spinner size='sm'/>} kcal</span>
+            </div>
+            <div className="w-full h-3 bg-white/20 rounded-full overflow-hidden">
+                <div className="h-full bg-yellow-400 transition-all duration-500" style={{ width: `${progressPercent}%` }}></div>
+            </div>
+        </section>
+    );
 }
