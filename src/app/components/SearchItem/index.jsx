1028e15735077f5dbb36f401b960a5bc2646c4e1
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-08 10:22:24 +0200

    renamed from js to jsx, added css modules

--- src/app/components/SearchItem/index.jsx
+++ src/app/components/SearchItem/index.jsx
@@ -1,15 +1,16 @@
 import { History } from 'lucide-react';
 import Link from 'next/link';
+import styles from './searchItem.module.css';
 
 export default function SearchItem({ id, name, brand, mealId }) {
   // Hvis vi kom fra et spesifikt måltid, sender vi med mealId videre til detaljsiden
   const href = mealId ? `/details/${id}?mealId=${mealId}` : `/details/${id}`;
 
   return (
-    <Link href={href} className="flex items-center gap-4 group">
-      <History className="text-[#00ffb3] opacity-70" size={24} />
-      <span className="text-lg group-hover:underline text-[#00ffb3]">
-        {name} <span className="opacity-60 text-sm">- {brand}</span>
+    <Link href={href} className={styles.itemLink}>
+      <History className={styles.icon} size={24} />
+      <span className={styles.title}>
+        {name} {brand && <span className={styles.brand}>- {brand}</span>}
       </span>
     </Link>
   );
