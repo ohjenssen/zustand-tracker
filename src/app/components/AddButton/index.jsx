854204ccbfd8ca52163eee92a1468fbcc62bc035
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-08 10:10:07 +0200

    Added dynamic AddButton

---
+++ src/app/components/AddButton/index.jsx
@@ -0,0 +1,17 @@
+import styles from './addButton.module.css';
+import Link from 'next/link';
+import { Plus } from 'lucide-react';
+
+export default function AddButton({href}){
+    return (
+        <div className={styles.fabWrapper}>
+            <Link 
+                // href={`/search?mealId=${currentMeal.id}`} 
+                href={href} 
+                className={styles.fabButton}
+            >
+                <Plus size={32} strokeWidth={3} />
+            </Link>
+        </div>
+    )
+}
