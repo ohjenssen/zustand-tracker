f5eff34034144aa0c09310a4af3314e98d879d5e
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-09 09:49:37 +0200

    cleaned up paage

--- src/app/details/[id]/page.js
+++ src/app/details/[id]/page.js
@@ -1,20 +1,13 @@
-import { getSpecificFood } from '@/app/utils/getSpecificFood';
 import DetailsClientForm from './DetailsClientForm';
-
 
 export default async function DetailsPage({ params }) {
   const { id } = await params; 
 
-  const food = await getSpecificFood(id) || { name: "Ukjent", kcal: 0, protein: 0, fat: 0, carbs: 0 };
-
   return (
     <main className="min-h-screen bg-[#003d2b] text-white p-6 pb-32 flex flex-col font-sans">
-      <h1 className="text-3xl font-bold text-center text-[#00ffb3] mt-12 mb-16">
-        {food.name} <span className="text-sm block text-white opacity-50">(Rendret på Server)</span>
-      </h1>
 
-      <DetailsClientForm food={food} />
+      <DetailsClientForm id={id}/>
+    
     </main>
   );
 }
-
