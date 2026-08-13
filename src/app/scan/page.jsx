614b12530f02539f7b7e8b9ccd9188fc1cf0422a
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-13 20:40:17 +0200

    Added spinner and loading state

--- src/app/scan/page.jsx
+++ src/app/scan/page.jsx
@@ -4,34 +4,33 @@ import { Suspense } from 'react';
 import NativeBarcodeScanner from '../components/NativeBarcodeScanner';
 import styles from './scanPage.module.css';
 import { useRouter } from 'next/navigation';
+import Spinner from '../components/Spinner';
 
 export default function ScanPage() {
   const [scannedCode, setScannedCode] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
   const router = useRouter();
 
   const handleScanSuccess = async (barcode) => {
-    console.log("Strekkode funnet:", barcode);
     setScannedCode(barcode);
     router.push(`/add-product?barcode=${barcode}`);
-    // Eksempel: Søk opp produktet i din egen backend via strekkoden
-    // const res = await fetch(`/api/food-products/barcode/${barcode}`);
-    // if (res.ok) {
-    //    const product = await res.json();
-    //    router.push(`/details/${product.id}`);
-    // }
   };
 
   return (
     <Suspense>
         <main>
             <section className={styles.scannerArea}>
-                <h1 className={styles.title}>Skann strekkode</h1>
-                
                 {!scannedCode && (
-                    <div className={styles.scannerWrapper}>
-                        <NativeBarcodeScanner onScanSuccess={handleScanSuccess} />
-                    </div>
+                    <>
+                        <h1 className={styles.title}>Skann strekkode</h1>
+                        <div className={styles.scannerWrapper}>
+                            <NativeBarcodeScanner onScanSuccess={handleScanSuccess} />
+                        </div>
+                    </>
                 )}
+                
+                {isLoading && <Spinner />}
         </section>
         </main>
     </Suspense>
