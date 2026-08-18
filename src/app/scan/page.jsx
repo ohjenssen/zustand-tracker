967e26dfa3b769011f926e99eb553a9f9389b0f5
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-18 20:18:08 +0200

    Added dynamic barcode to url when scanning

--- src/app/scan/page.jsx
+++ src/app/scan/page.jsx
@@ -5,16 +5,33 @@ import NativeBarcodeScanner from '../components/NativeBarcodeScanner';
 import styles from './scanPage.module.css';
 import { useRouter } from 'next/navigation';
 import Spinner from '../components/Spinner';
+import { useAuthStore } from '../store/store';
 
 export default function ScanPage() {
-  const [scannedCode, setScannedCode] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+    const [scannedCode, setScannedCode] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const token = useAuthStore((state) => state.token);
 
   const router = useRouter();
 
   const handleScanSuccess = async (barcode) => {
     setScannedCode(barcode);
-    router.push(`/add-product?barcode=${barcode}`);
+    try {
+        const res = await fetch(`https://foodtracker-api.oskarjenssen.com/api/food-products?barcode=${barcode}`, {
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': `Bearer ${token}`
+                }
+        });
+
+        const json = await res.json();
+        
+        if(json.length === 0){
+            router.push(`/add-product?barcode=${barcode}`);
+        }
+    } catch {
+
+    }
   };
 
   return (
