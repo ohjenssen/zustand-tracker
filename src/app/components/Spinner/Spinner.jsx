ded8caf01a4cc547b9f8a2e1813ca2712967b60c
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-19 20:35:49 +0200

    Updated spinner animation

--- src/app/components/Spinner/Spinner.jsx
+++ src/app/components/Spinner/Spinner.jsx
@@ -1,11 +1,10 @@
 import styles from './spinner.module.css';
 
-export default function Spinner({ size = 'md', color }) {
-  // Mapper størrelsene til klassene i CSS-filen
+export default function Spinner({ size = 'md', color, isHiding = false }) {
   const sizeClass = styles[size] || styles.md;
 
   return (
-    <span className={styles.wrapper}>
+    <span className={`${styles.wrapper} ${isHiding ? styles.fadeOut : ''}`}>
       <div
         className={`${styles.spinner} ${sizeClass}`}
         style={color ? { color } : undefined}
