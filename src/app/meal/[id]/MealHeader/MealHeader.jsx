a969f5ecafc42e1a43d9d6697084eabfbfbaf909
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-20 21:06:28 +0200

    Remvoed backButton in header

--- src/app/meal/[id]/MealHeader/MealHeader.jsx
+++ src/app/meal/[id]/MealHeader/MealHeader.jsx
@@ -1,13 +1,8 @@
-import { ChevronLeft } from 'lucide-react';
-import Link from 'next/link';
 import styles from './mealHeader.module.css';
 
 export default function MealHeader({ name }) {
   return (
     <header className={styles.header}>
-        <Link href="/home" className={styles.backLink}>
-            <ChevronLeft size={32} />
-        </Link>
         <h1 className={styles.title}>{name}</h1>
         <div className={styles.spacer}></div>
     </header>
