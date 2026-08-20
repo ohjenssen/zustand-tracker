fab7358db8880dcd847ac4be3d565951d9364723
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-20 20:18:00 +0200

    Moved SearchContent into search-page

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import SearchContent from '@/app/components/SearchContent';
+import SearchContent from '@/app/search/SearchContent';
 
 export default function SearchPage() {
 	return (
