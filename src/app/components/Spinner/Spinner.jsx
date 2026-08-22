973a529c62ad22244f480c18b6099a82e42afe70
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-22 17:16:57 +0200

    Added conditional loadingContainer to spinner

--- src/app/components/Spinner/Spinner.jsx
+++ src/app/components/Spinner/Spinner.jsx
@@ -1,17 +1,33 @@
 import styles from './spinner.module.css';
 
-export default function Spinner({ size = 'md', color, isHiding = false }) {
+export default function Spinner({container = false, size = 'md', color, isHiding = false }) {
   const sizeClass = styles[size] || styles.md;
 
   return (
-    <span className={`${styles.wrapper} ${isHiding ? styles.fadeOut : ''}`}>
-      <div
-        className={`${styles.spinner} ${sizeClass}`}
-        style={color ? { color } : undefined}
-        role="status"
-      >
-        <span className={styles.srOnly}>Laster inn...</span>
-      </div>
-    </span>
+    <>
+        {container ? 
+        <div className={styles.loadingContainer}>
+            <span className={`${styles.wrapper} ${isHiding ? styles.fadeOut : ''}`}>
+                <div
+                className={`${styles.spinner} ${sizeClass}`}
+                style={color ? { color } : undefined}
+                role="status"
+                >
+                    <span className={styles.srOnly}>Laster inn...</span>
+                </div>
+            </span> 
+        </div> : 
+        
+        <span className={`${styles.wrapper} ${isHiding ? styles.fadeOut : ''}`}>
+            <div
+                className={`${styles.spinner} ${sizeClass}`}
+                style={color ? { color } : undefined}
+                role="status"
+                >
+                <span className={styles.srOnly}>Laster inn...</span>
+            </div>
+        </span>
+        }
+    </>
   );
 }
