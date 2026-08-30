ac222c1fd6205fe5b081f11d2d36354c0e1c4b85
Oskar <oskar.heming.jenssen@gmail.com>
2026-08-30 21:05:25 +0200

    Converted tailwind to css

--- src/app/details/[id]/page.jsx
+++ src/app/details/[id]/page.jsx
@@ -1,12 +1,12 @@
 import BackButton from '@/app/components/BackButton/BackButton';
 import DetailsClientForm from './DetailsClientForm/DetailsClientForm';
+import styles from './page.module.css';
 
 export default async function DetailsPage({ params }) {
   const { id } = await params; 
 
   return (
-    <main className="min-h-screen bg-[#003d2b] text-white p-6 pb-32 flex flex-col font-sans">
-
+    <main className={styles.main}>
       <DetailsClientForm id={id}/>
       <BackButton />
     </main>
